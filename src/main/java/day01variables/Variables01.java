81e00f844876d70d0470d7c3c74ccb03dc1673d7
Ziarahman Musleh <ziarahmanmusleh@gmail.com>
2023-03-19 21:30:24 +0300

    day01

---
+++ src/main/java/day01variables/Variables01.java
@@ -0,0 +1,27 @@
+package day01variables;
+
+public class Variables01 {
+    //This is or first day in coding <== This is a note for people not for Java that is why I put "//"
+
+        /*
+            This method is not working as expected.
+            I disabled this method.
+            When I fix it I will enable again
+        */
+
+    //How to create a variable
+    public int ageOfAli = 23;
+    public String nameOfAli = "Ali Can";
+
+    //Data Types in Java
+    //There are 2 main data types; i)Primitive Data Types   ii)Non-Primitive Data Types
+    /*
+            i)Primitive Data Types: byte - short - int - long - float - double - char - boolean
+                                    byte - short - int - long - float - double are numeric data types
+                                    char and boolean are not numeric data types
+            boolean: boolean data type has 2 values "true" or "false"
+                     boolean uses just "1 bit" in the memory
+     */
+    public boolean isRetired = true;
+
+}//class
